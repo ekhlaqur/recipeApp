53de9d58ca9354416b71d6978f24f46719577323
Ekhlaqur Rahman <ekhlaqur.rahman91@gmail.com>
2023-02-25 15:53:02 +0530

    file updated

--- Recipe.js
+++ Recipe.js
@@ -1,11 +1,78 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
-const Recipe = ({title,calories,image,ingridient}) => {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+}));
+const Recipe = ({title,calories,image,ingredient}) => {
+  const classes = useStyles();
+  const [expanded, setExpanded] = useState(false);
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
   return (
     <div>
-    <p>{title}</p>
-    <p>{calories}</p>
-    <img src={image} alt="" />
+    <Card className={classes.root}>
+      <CardHeader
+
+        title={title}
+        subheader="calories"
+      />
+      <CardMedia
+        className={classes.media}
+        image={image}
+        title={title}
+      />
+      <CardActions disableSpacing>
+      <div>ingredient</div>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+        {ingredient.map((ingredient) => {
+          <Typography paragraph>{ingredient.text}</Typography>
+        })}
+        </CardContent>
+      </Collapse>
+    </Card>
     </div>
   )
 }
--- app.js
+++ app.js
@@ -1,33 +1,83 @@
-/**
- * root file for api configuration
- */
-const express = require("express");
-const bodyParser = require("body-parser");
-const routes = require("./recipes.routes");
-const dbConfig = require("./database.config");
-const moongoose = require("mongoose");
+import axios from "axios";
+import React,{useEffect,useState} from "react";
+import './App.css';
+import Recipe from "./Recipe";
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import IconButton from '@material-ui/core/IconButton';
+import Grid from '@material-ui/core/Grid';
+import SearchIcon from '@material-ui/icons/Search';
 
-moongoose.Promise = global.Promise;
-moongoose
-  .connect(dbConfig.url)
-  .then(() => {
-    console.log("successfully connected to the database");
-  })
-  .catch(() => {
-    console.log("unable to connect to the database  Exiting now..");
-    process.exit();
-  });
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    width: 400,
+    margin:"12px auto"
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+}));
 
-// create express app
-const app = express();
+function App() {
+  const classes = useStyles();
+  const APPID = "aa537d20"
+  const APPKEY = "75c947f7fd942d3cbe0b7060334477af";
+  const [recipes, setRecipe] = useState([])
+  const [search, setSearch] = useState("");
+  [ask, setAsk] = useState("chicken")
+  useEffect(()=>{
+    getRecipe();
+  
+  }, [ask]);
+  const getRecipe = async() =>{
+    const response = await axios.get(
+      `http://api.edamam.com/search?q=${ask}&app_id=${APPID}&app_key=${APPKEY}`
+    );
+    setRecipe(response.data.hits);
+    console.log(response.data.hits)
+  }
+ const handlerSearch = () =>{
+  setSearch(e.target.value)
+    console.log(e.target.value)
+  }
+  const askUpdate = (e) =>{
+    e.preventDefault();
+    setAsk(search)
+  }
+  return(
+  <div>
+  <Paper onSubmit={askUpdate} component="form" className={classes.root}>
+      <InputBase
+      type="tex" value={search} onClick={handlerSearch}
+        className={classes.input}
+        placeholder="Search for recipe"
+        inputProps={{ 'aria-label': 'search for recipe' }}
+      />
+      <IconButton type="submit" className={classes.iconButton} aria-label="search">
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  <Grid wrapper>
+  {recipes.map((recipe) =>{
+    <Grid item xs={3}>
+    <Recipe 
+    key = {recipe.recipe.lable}
+    title = {recipe.recipe.lable} calories={recipe.recipe.calories} image={recipe.recipe.image} 
+    />
+    </Grid>
+  })}
+  </Grid>
+<Recipe/>
+  </div>
+  );
+}
 
-// parse requests of content-type - application/json
-app.use(bodyParser.json());
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-
-routes(app);
-// listen for requests
-const server = app.listen(3004, function() {
-  console.log("app running on", server.address().port);
-});
+export default App;
